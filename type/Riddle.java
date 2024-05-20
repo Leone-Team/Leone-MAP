ff0183abbee21090aa04a1f3a56c1a5da6e235fd
dtinelli12 <61162306+dtinelli12@users.noreply.github.com>
2024-05-20 18:58:23 +0200

    Carico classe Riddle

---
+++ type/Riddle.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package di.uniba.map.type;
+
+/**
+ *
+ * @author giann
+ */
+public class Riddle {
+    private static int ID;
+    private String question;
+    private String answer;
+    private boolean solved;
+
+    public Riddle(String question, String answer, boolean solved) {
+        this.question = question;
+        this.answer = answer;
+        this.solved = solved;
+    }
+
+    public Riddle() {
+    }
+    
+    public boolean checkAnswer(String answer) {
+        if (answer == this.question)
+            return true;
+        else
+            return false;
+    }
+}
