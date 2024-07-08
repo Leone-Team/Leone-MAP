d83e23d11d9be09a01880f6691e7330271bdec72
utente <utente@DESKTOP-QP1N476>
2024-07-08 11:56:51 +0200

    Inserimento della classifica dei giocatori in GameTime

--- src/main/java/di/uniba/leone/game/GameTime.java
+++ src/main/java/di/uniba/leone/game/GameTime.java
@@ -5,11 +5,22 @@
 package di.uniba.leone.game;
 
 import java.time.LocalTime;
+import java.util.List;
+import java.util.Scanner;
 
 public class GameTime implements Runnable {
         private LocalTime timeBegin;
         private LocalTime timeEnd;
-        
+        private String nickname;
+
+        public GameTime(String nickname) {
+            this.nickname = nickname;
+        }
+
+    GameTime() {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
         public void start() {
             this.timeBegin = LocalTime.now();
         }
@@ -17,13 +28,15 @@ public class GameTime implements Runnable {
         public void stop() {
             this.timeEnd = LocalTime.now();
         }
-        
+
         public void run() {
+            synchronized (this) {
             try {
                 wait();
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
                 System.out.println("Il thread è stato interrotto.");
+            }
             }
         }
         
@@ -31,5 +44,29 @@ public class GameTime implements Runnable {
             int count = timeEnd.toSecondOfDay() - timeBegin.toSecondOfDay();
             System.out.println("La durata della partita e' stata di: " +LocalTime.ofSecondOfDay(count));
         }
+        
+        public long getCount() {
+            if (timeBegin == null || timeEnd == null) {
+                return Long.MAX_VALUE;
+            }
+            return timeEnd.toSecondOfDay() - timeBegin.toSecondOfDay();
+        }
+        
+        public void classification(List<GameTime> players) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Inserire il nickname: ");
+            String nickname = scanner.nextLine();
+      
+            players.sort((c1, c2) -> Long.compare(c1.getCount(), c2.getCount()));
+ 
+            System.out.println("La classifica dei giocatori e': ");
+            for (GameTime c : players) {
+            if (c.getCount() != Long.MAX_VALUE) {
+                System.out.println(c.nickname + ": " + c.getCount());
+            } else {
+                    System.out.println(c.nickname + " non ha concluso il gioco, che fifone/a!!");
+                }
+            }
+        }  
 }
 
